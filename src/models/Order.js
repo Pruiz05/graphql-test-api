9e95699ea1b240a69435aeb42de20c95ec880e7b
PedroLuis <pedroluis0542@gmail.com>
2021-05-24 21:38:47 -0400

    update packages and testing mutations and querys

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,34 +1,32 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
-    order : {
-        type: Array,
-        required: true
-    },
-    total: {
-        type: Number,
-        required: true
-    },
-    client: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Client'
-    },
-    vendor: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-    state: {
-        type: String,
-        default: 'PENDIENTE'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
+  order: {
+    type: Array,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Client",
+  },
+  vendor: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  state: {
+    type: String,
+    default: "PENDIENTE",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-})
-
-module.exports = mongoose.model('Order', OrderSchema)
+module.exports = mongoose.model("Order", OrderSchema);
